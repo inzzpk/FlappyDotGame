26a820bafe64395303732f6e0d444aefcf4687ee
InzzPK <inzz.pk@gmail.com>
2014-09-03 19:05:58 +0700

    Built Method

---
+++ src/flappydot/FlappyDotGame.java
@@ -0,0 +1,34 @@
+package flappydot;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+	public class FlappyDotGame extends BasicGame {
+		 
+		  public FlappyDotGame(String title) {
+		    super(title);
+		  }
+
+		@Override
+		public void render(GameContainer arg0, Graphics arg1)
+				throws SlickException {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void init(GameContainer arg0) throws SlickException {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void update(GameContainer arg0, int arg1) throws SlickException {
+			// TODO Auto-generated method stub
+			
+		}
+		 
+		  // ... เพิ่มเอง
+		}
