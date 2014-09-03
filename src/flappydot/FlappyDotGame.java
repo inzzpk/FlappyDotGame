43df2456abd1b97314ad971d5a9a16f66bc94642
InzzPK <inzz.pk@gmail.com>
2014-09-03 19:54:53 +0700

    Dot can jamp with space bar and fall

--- src/flappydot/FlappyDotGame.java
+++ src/flappydot/FlappyDotGame.java
@@ -5,13 +5,14 @@ import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 	public class FlappyDotGame extends BasicGame {
 		  public static final int GAME_WIDTH = 640;
 		  public static final int GAME_HEIGHT = 480;
-		  public static final float DOT_INITIAL_VY = -10;
-		  public static final float G = (float) 0.5;
+		  public static final float DOT_INITIAL_VY = 10;
+		  public static final float G = (float) -0.5;
 		  private Dot dot;
 
 		public FlappyDotGame(String title) {
@@ -30,7 +31,7 @@ import org.newdawn.slick.SlickException;
 		    Color background = new Color(128, 128, 128);
 		    container.getGraphics().setBackground(background); 
 	//	    dot = new Dot(GAME_WIDTH/2, GAME_HEIGHT/2);
-		    dot = new Dot(GAME_WIDTH/2, GAME_HEIGHT/2, DOT_INITIAL_VY);
+		    dot = new Dot(GAME_WIDTH/2, GAME_HEIGHT/2, -(DOT_INITIAL_VY));
 			
 		}
 
@@ -39,6 +40,13 @@ import org.newdawn.slick.SlickException;
 			dot.update();
 			
 		}
+		
+		@Override
+		  public void keyPressed(int key, char c) {
+			if (key == Input.KEY_SPACE) {
+			      dot.setVy(DOT_INITIAL_VY);
+			    }
+		  }
 		 
 		 public static void main(String[] args) {
 			    try {
